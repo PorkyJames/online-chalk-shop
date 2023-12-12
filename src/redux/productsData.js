eeae27523cf0f3d2ef83987bdab50a9cc77921f5
PorkyJames <kwanjko17@gmail.com>
2023-12-12 13:17:20 -0800

    First Push

---
+++ src/redux/productsData.js
@@ -0,0 +1,10 @@
+const productsData = [
+    {name: "Asana White Dirt Climbing Chalk (12oz, 340g) - Powder ", price: 16, img:"https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61Ba+yptS9L._AC_UL320_.jpg"},
+    {name: "Asana White Dirt Climbing Chalk (4oz, 125ml) - Liquid", price: 13, img:"https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51DEw8kKmmL._AC_UL320_.jpg"},
+    {name: "Magdust Chalk - Premium 100% Magnesium Carbonate", price: 22, img:"https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51X06Q1Z1PL._AC_UL320_.jpg"},
+    {name: "FrictionLabs Premium Sports Chalk", price: 10, img:"https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81+pdiYAvJL._AC_UL320_.jpg"},
+    {name: "FrictionLabs Magic Chalk Ball 2.2oz", price: 13, img:"https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81Qszz8-WJL._AC_UL320_.jpg"},
+    {name: "FrictionLabs Secret Stuff Liquid Chalk 75mL", price: 19, img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41SKve+iU4L._AC_UL320_.jpg"}
+]
+
+export default productsData;
