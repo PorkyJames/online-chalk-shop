69b89647670bf28bbfd1627b6a996b868e9c1e24
PorkyJames <kwanjko17@gmail.com>
2023-12-19 14:34:18 -0800

    Created ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,25 @@
 import React from "react";
+import {connect} from "react-redux"
+import { addToCart } from "../redux/action";
 
-const ProductList = () => {
+const ProductList = ({products, addToCart}) => {
     return (
-        <h1> Product List </h1>
+        <div className = "productList">
+            <h2 id="title">Chalk</h2>
+
+            <ul className='products'>
+                {products.map(product => (
+                    <div key={product.name} className="product">
+                        <img src={product.img} />
+                        <div className="productDetails">
+                            <h3>{product.name}</h3>
+                            <p id ="productPrice">Price: {product.price}</p>
+                            <button id="buyBtn" onClick={() => addToCart(product)}></button>
+                        </div>
+                    </div>
+                ))}
+            </ul>
+        </div>
     )
 }
 
